9692f20781a08563f0499fd8a9f25ecc53356ad6
Deepika Malini Rajasekar <deepirajsekar03@gmail.com>
2022-10-24 11:51:13 +0300

    add input text file

---
+++ src/main/java/com/puzzlesolver/App.java
@@ -0,0 +1,13 @@
+package com.puzzlesolver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
